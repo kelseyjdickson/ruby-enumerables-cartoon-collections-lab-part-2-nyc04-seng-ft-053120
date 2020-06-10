60b3a7f5e45ff4d139a998b36a3a7081077e0af6
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-10 15:03:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
@@ -29,6 +31,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |x|
     x == valid_calls
+    binding.pry
 
 
   end
